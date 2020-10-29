b7f90d7d5a5298802aad3b44c92a8daea4cc1bbc
Justyna Gapys <justynagapys0310@gmail.com>
2020-10-29 12:05:43 +0100

    Tablice-Commit2

--- Tablice i Przetwarzanie tekstu/Zadania/src/tablice.php
+++ Tablice i Przetwarzanie tekstu/Zadania/src/tablice.php
@@ -3,7 +3,39 @@
 </div>
 <div class="flex flex-row flex-wrap w-full justify-center text-xl mb-2">
   <?php
-    
+    $tablica = array("pierwszy", "drugi", "czwarty");
+    echo $tablica[2];
+    echo "</br>";
+
+    foreach ($tablica as $wartosc){
+      echo $wartosc, "</br>";
+    }
+
+    print_r($tablica);
+    echo "</br>";
+
+    echo count($tablica), '</br>';
+
+    $nowaTablica = array("klucz1"=>"wartoscJakas", "klucz2"=>"lepszaWartosc");
+    echo $nowaTablica["klucz2"], "</br>";
+
+    $nowaTablica["klucz2"]="innaWartosc";
+    echo $nowaTablica["klucz2"], "</br>";
+
+    $reversed=array_reverse($nowaTablica);
+    print_r($reversed);
+    echo "</br>";
+
+    function sprawdzCzyJest($wartoscSzukana, $tablica){
+      if(in_array($wartoscSzukana, $tablica)){
+        echo "jest", "</br>";
+      } else{
+      echo "nie ma", "</br>";
+      }
+    }
+
+    sprawdzCzyJest("czwarty", $tablica);
+    sprawdzCzyJest("piąty", $tablica);
 
   ?>
 <div>
