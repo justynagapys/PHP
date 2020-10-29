659a09bd8ccdfbdc811ced43ac3213a7ef455beb
Justyna Gapys <justynagapys0310@gmail.com>
2020-10-29 11:37:48 +0100

    TabliceITekst-Commit1

---
+++ Tablice i Przetwarzanie tekstu/Zadania/src/tekst.php
@@ -0,0 +1,9 @@
+<div class="flex flex-row flex-wrap w-full justify-center font-bold text-3xl mb-2">
+  Tekst
+</div>
+<div class="flex flex-row flex-wrap w-full justify-center text-xl mb-2">
+  <?php
+    
+
+  ?>
+<div>
