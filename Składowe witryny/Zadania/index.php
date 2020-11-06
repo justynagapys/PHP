5d9018a577ba0bfb33d503c8372b40822761932a
Justyna Gapys <justynagapys0310@gmail.com>
2020-11-06 10:50:08 +0100

    skladoweWitrynyCommit1

---
+++ Składowe witryny/Zadania/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Składowe witryny</title>
+</head>
+<body>
+    <form method="GET">
+        <div>
+            metoda GET
+        </div>
+        <div>
+            <label for="imie">Imię: </label>
+            <input name="imie">
+        </div>
+        <div>
+            <label for="nazwisko">Nazwisko: </label>
+            <input name="nazwisko">
+        </div>
+        <div>
+            <label for="email">E-mail: </label>
+            <input name="email">
+        </div>
+        <button type="submit" name="zatwierdz" value="zatwierdzenie">
+            Wyślij
+        </button>
+    </form>
+</body>
+</html>
