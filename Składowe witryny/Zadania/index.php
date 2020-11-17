8114a8c2b457299fad241b4656e6f0fe956be5b6
Justyna Gapys <justynagapys0310@gmail.com>
2020-11-17 17:04:50 +0100

    skladoweWitrynyCommit5Sesja

--- Składowe witryny/Zadania/index.php
+++ Składowe witryny/Zadania/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Składowe witryny</title>
-    <link href="https://unpkg.com/tailwindcss@1.8.10/dist/tailwind.css" rel="stylesheet">
+    <!-- <link href="https://unpkg.com/tailwindcss@1.8.10/dist/tailwind.css" rel="stylesheet"> -->
 </head>
 
 <body>
--- Składowe witryny/Zadania/src/sesjaPOST.php
+++ Składowe witryny/Zadania/src/sesjaPOST.php
@@ -1,18 +1,8 @@
 <?php
 session_start();
-
+$session_value='nazwisko';
 if ($_POST['zatwierdz']) {
-    $_SESSION['session_value'] = ($_SESSION['nazwisko']);
+    $_SESSION['nazwisko'] = $_POST[$session_value];
 }
 header("Location: " . $_SERVER['HTTP_REFERER']);
 ?>
-
-W katalogu src utwórz plik sesjaPOST.php, a w pliku index.php stwórz formularz typu POST z polem nazwisko posiadający action z odniesieniem do wcześniej stworzonego pliku.
-
-<!-- Zapisz wartość pola nazwisko. 
-Wykorzystaj zmienną superglobalną $_POST i sprawdź 
-czy istnieje wartość zatwierdz, 
-jeżeli istnieje utwórz w sesji poprzez zmienną $_SESSION pole nazwisko 
-i przypisz wartość pola nazwisko. 
-Przed odwołaniem się do sesji należy ja uruchomić wywołaniem funkcji session_start. 
-Przekieruj użytkownika na stronę główną instrukcją header. -->
