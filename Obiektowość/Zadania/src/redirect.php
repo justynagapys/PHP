0523d382809a9fccf9c25ff739c8f89eb7d0527d
Justyna Gapys <justynagapys0310@gmail.com>
2020-11-27 00:34:35 +0100

    ObiektowoscCommit3Redirect

--- Obiektowość/Zadania/src/redirect.php
+++ Obiektowość/Zadania/src/redirect.php
@@ -1,3 +1,11 @@
 <?php
+    // zapisanie danych w sesji i przeniesienie do kolejnego formularza
 
+    session_start();
+
+    $_SESSION['kwota'] = $_POST['kwota'];
+    $_SESSION['okres'] = $_POST['okres'];
+
+    header("Location: form.php");
+    exit;
 ?>
